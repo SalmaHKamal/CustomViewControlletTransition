d0466868e72638ede0a1a8141a3be850f20c189f
Salma Hassan <salmahassan087@gmail.com>
2020-07-17 08:26:26 +0200

    Save working navigation height

--- NavigationTransition/New Group/OneViewController.swift
+++ NavigationTransition/New Group/OneViewController.swift
@@ -8,43 +8,26 @@
 
 import UIKit
 
-//extension UINavigationBar {
-//
-//    override open func sizeThatFits(_ size: CGSize) -> CGSize {
-//        return CGSize(width: UIScreen.main.bounds.size.width, height: 80.0)
-//    }
-//
-//}
-
 class OneViewController: UIViewController { //, CAAnimationDelegate
 
-   override func viewDidAppear(_ animated: Bool) {
-//      navigationController?.navigationBar.sizeThatFits(CGSize(width: UIScreen.main.bounds.size.width, height: 80.0))
-//      for subview in (self.navigationController?.navigationBar.subviews)! {
-//         if NSStringFromClass(subview.classForCoder).contains("BarBackground") {
-//              var subViewFrame: CGRect = subview.frame
-//              // subViewFrame.origin.y = -20;
-//              subViewFrame.size.height = 100
-//              subview.frame = subViewFrame
-//
-//          }
-//
-//      }
-   }
-   
     override func viewDidLoad() {
         super.viewDidLoad()
       
       navigationController?.delegate = self
+      self.navigationController?.setNavigationBarHidden(false, animated: false)
+      let customNavigationBar = UIView(frame: CGRect(x: navigationController?.navigationBar.bounds.minX ?? 0, y: navigationController?.navigationBar.bounds.minY ?? 0 , width: navigationController?.navigationBar.bounds.width ?? 0, height: 100))
+      customNavigationBar.backgroundColor = .green
+      navigationController?.navigationBar.addSubview(customNavigationBar)
+//      self.navigationBar.addSubview(customNavigationBar)
     }
     
 
    @IBAction func btnPressed(){
       let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "two")
       navigationController?.pushViewController(vc, animated: true)
+      print(navigationController?.navigationBar.frame)
    }
    
-
 }
 
 
@@ -55,31 +38,7 @@ extension OneViewController: UINavigationControllerDelegate {
       
       let navigationBarAnimation = CATransition()
       navigationBarAnimation.duration = 100
-//      navigationBarAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeIn)
-//      navigationBarAnimation.type = CATransitionType.push
-////      navigationBarAnimation.subtype = kCATransitionFade
-//      navigationBarAnimation.isRemovedOnCompletion = true
       self.navigationController?.navigationBar.layer.add(navigationBarAnimation, forKey: nil)
-      
-////      UIView.transition(from: (self.navigationController?.visibleViewController!.view)!, to: viewController.view, duration: 0.5, options: .curveEaseOut) { (_) in
-////         <#code#>
-////      }
-//
-//
-////      let transition = CATransition()
-////      transition.duration = 0.3
-////        transition.type = CATransitionType.push
-////      transition.subtype = UINavigationController.Operation.push ? CATransitionSubtype.fromRight : CATransitionSubtype.fromRight
-////        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
-//////      transition.delegate = self
-////      self.navigationController!.visibleViewController?.view.layer.add(transition, forKey: kCATransition)
-//      let navigationBarAnimation = CATransition()
-//      navigationBarAnimation.duration = 100
-//      navigationBarAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeIn)
-//      navigationBarAnimation.type = CATransitionType.push
-////      navigationBarAnimation.subtype = CATransitionType.fade
-////      navigationBarAnimation.isRemovedOnCompletion = true
-//      navigationController.navigationBar.layer.add(navigationBarAnimation, forKey: nil)
    }
 
    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
@@ -87,4 +46,10 @@ extension OneViewController: UINavigationControllerDelegate {
    }
 }
 
+class CustomNavigationBar: UINavigationBar {
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        let newSize :CGSize = CGSize(width: self.frame.size.width, height: 200)
+        return newSize
+    }
+}
 
